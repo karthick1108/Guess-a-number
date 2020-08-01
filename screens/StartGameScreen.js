b1866cafb06b7b424c3a28500c2dd4a6e3a082a9
Karthick Rajasekaran <kraj0008@student.monash.edu>
2020-08-01 14:41:24 +1000

    Created separate component for card

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -1,18 +1,20 @@
 import React from "react";
-import { View, StyleSheet, Text, TextInput, Button } from "react-native";
+import { View, Text, StyleSheet, TextInput, Button } from "react-native";
 
-const GameScreen = (props) => {
+import Card from "../components/Card";
+
+const StartGameScreen = (props) => {
   return (
     <View style={styles.screen}>
       <Text style={styles.title}>Start a New Game!</Text>
-      <View style={styles.inputContainer}>
+      <Card style={styles.inputContainer}>
         <Text>Select a Number</Text>
         <TextInput />
         <View style={styles.buttonContainer}>
           <Button title="Reset" onPress={() => {}} />
           <Button title="Confirm" onPress={() => {}} />
         </View>
-      </View>
+      </Card>
     </View>
   );
 };
@@ -40,4 +42,4 @@ const styles = StyleSheet.create({
   },
 });
 
-export default GameScreen;
+export default StartGameScreen;
