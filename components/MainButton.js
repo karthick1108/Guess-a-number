8af9a5e18634a7e194d68e5152edacb85ea3b0c1
Karthick Rajasekaran <kraj0008@student.monash.edu>
2020-08-06 18:18:44 +1000

    added scrollview

--- components/MainButton.js
+++ components/MainButton.js
@@ -1,10 +1,11 @@
-import React from "react";
-import { View, Text, StyleSheet, TouchableOpacity } from "react-native";
-import Colors from "../constants/colors";
+import React from 'react';
+import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
 
-const MainButton = (props) => {
+import Colors from '../constants/colors';
+
+const MainButton = props => {
   return (
-    <TouchableOpacity onPress={props.onPress}>
+    <TouchableOpacity activeOpacity={0.6} onPress={props.onPress}>
       <View style={styles.button}>
         <Text style={styles.buttonText}>{props.children}</Text>
       </View>
@@ -17,12 +18,13 @@ const styles = StyleSheet.create({
     backgroundColor: Colors.primary,
     paddingVertical: 12,
     paddingHorizontal: 30,
-    borderRadius: 25,
+    borderRadius: 25
   },
   buttonText: {
-    color: "white",
-    fontFamily: "open-sans",
-    fontSize: 18,
-  },
+    color: 'white',
+    fontFamily: 'open-sans',
+    fontSize: 18
+  }
 });
+
 export default MainButton;
--- screens/GameOverScreen.js
+++ screens/GameOverScreen.js
@@ -1,34 +1,35 @@
-import React from "react";
-import { View, Text, StyleSheet, Button, Image } from "react-native";
+import React from 'react';
+import { View, Text, StyleSheet, Button, Image } from 'react-native';
 
-import BodyText from "../components/BodyText";
-import TitleText from "../components/TitleText";
-import Colors from "../constants/colors";
-import MainButton from "../components/MainButton";
+import BodyText from '../components/BodyText';
+import TitleText from '../components/TitleText';
+import MainButton from '../components/MainButton';
+import Colors from '../constants/colors';
 
-const GameOverScreen = (props) => {
+const GameOverScreen = props => {
   return (
     <View style={styles.screen}>
       <TitleText>The Game is Over!</TitleText>
       <View style={styles.imageContainer}>
         <Image
-          source={require("../assets/success.png")}
-          //source={{
-          //  uri:
-          //    "https://cdn.pixabay.com/photo/2016/05/05/23/52/mountain-summit-1375015_960_720.jpg",
-          //}}
+          source={require('../assets/success.png')}
+          // source={{
+          //   uri:
+          //     'https://cdn.pixabay.com/photo/2016/05/05/23/52/mountain-summit-1375015_960_720.jpg'
+          // }}
           style={styles.image}
           resizeMode="cover"
         />
       </View>
       <View style={styles.resultContainer}>
         <BodyText style={styles.resultText}>
-          Your phone needed{" "}
+          Your phone needed{' '}
           <Text style={styles.highlight}>{props.roundsNumber}</Text> rounds to
-          guess the number{" "}
-          <Text style={styles.highlight}>{props.userNumber}</Text>
+          guess the number{' '}
+          <Text style={styles.highlight}>{props.userNumber}</Text>.
         </BodyText>
       </View>
+
       <MainButton onPress={props.onRestart}>NEW GAME</MainButton>
     </View>
   );
@@ -37,34 +38,34 @@ const GameOverScreen = (props) => {
 const styles = StyleSheet.create({
   screen: {
     flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
+    justifyContent: 'center',
+    alignItems: 'center'
   },
   imageContainer: {
     width: 300,
     height: 300,
     borderRadius: 150,
     borderWidth: 3,
-    borderColor: "black",
-    overflow: "hidden",
-    marginVertical: 30,
+    borderColor: 'black',
+    overflow: 'hidden',
+    marginVertical: 30
   },
   image: {
-    width: "100%",
-    height: "100%",
+    width: '100%',
+    height: '100%'
   },
   resultContainer: {
     marginHorizontal: 30,
-    marginVertical: 15,
+    marginVertical: 15
+  },
+  resultText: {
+    textAlign: 'center',
+    fontSize: 20
   },
   highlight: {
     color: Colors.primary,
-    fontFamily: "open-sans-bold",
-  },
-  resultText: {
-    textAlign: "center",
-    fontSize: 15,
-  },
+    fontFamily: 'open-sans-bold'
+  }
 });
 
 export default GameOverScreen;
