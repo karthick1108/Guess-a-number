1dda42fc4e96e0f6820428a09e4236f1195f40c4
Karthick Rajasekaran <kraj0008@student.monash.edu>
2020-08-06 17:10:25 +1000

    Styled game over screen

--- screens/GameOverScreen.js
+++ screens/GameOverScreen.js
@@ -1,22 +1,33 @@
-import React from 'react';
-import { View, Text, StyleSheet, Button, Image } from 'react-native';
+import React from "react";
+import { View, Text, StyleSheet, Button, Image } from "react-native";
 
-import BodyText from '../components/BodyText';
-import TitleText from '../components/TitleText';
+import BodyText from "../components/BodyText";
+import TitleText from "../components/TitleText";
+import Colors from "../constants/colors";
 
-const GameOverScreen = props => {
+const GameOverScreen = (props) => {
   return (
     <View style={styles.screen}>
       <TitleText>The Game is Over!</TitleText>
       <View style={styles.imageContainer}>
         <Image
-          source={require('../assets/success.png')}
+          source={require("../assets/success.png")}
+          //source={{
+          //  uri:
+          //    "https://cdn.pixabay.com/photo/2016/05/05/23/52/mountain-summit-1375015_960_720.jpg",
+          //}}
           style={styles.image}
           resizeMode="cover"
         />
       </View>
-      <BodyText>Number of rounds: {props.roundsNumber}</BodyText>
-      <BodyText>Number was: {props.userNumber}</BodyText>
+      <View style={styles.resultContainer}>
+        <BodyText style={styles.resultText}>
+          Your phone needed{" "}
+          <Text style={styles.highlight}>{props.roundsNumber}</Text> rounds to
+          guess the number{" "}
+          <Text style={styles.highlight}>{props.userNumber}</Text>
+        </BodyText>
+      </View>
       <Button title="NEW GAME" onPress={props.onRestart} />
     </View>
   );
@@ -25,22 +36,34 @@ const GameOverScreen = props => {
 const styles = StyleSheet.create({
   screen: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center'
+    justifyContent: "center",
+    alignItems: "center",
   },
   imageContainer: {
     width: 300,
     height: 300,
     borderRadius: 150,
     borderWidth: 3,
-    borderColor: 'black',
-    overflow: 'hidden',
-    marginVertical: 30
+    borderColor: "black",
+    overflow: "hidden",
+    marginVertical: 30,
   },
   image: {
-    width: '100%',
-    height: '100%'
-  }
+    width: "100%",
+    height: "100%",
+  },
+  resultContainer: {
+    marginHorizontal: 30,
+    marginVertical: 15,
+  },
+  highlight: {
+    color: Colors.primary,
+    fontFamily: "open-sans-bold",
+  },
+  resultText: {
+    textAlign: "center",
+    fontSize: 15,
+  },
 });
 
 export default GameOverScreen;
